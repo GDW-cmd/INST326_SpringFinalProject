972e7a9420e79424ef71909e503a635fa1e044f7
MoGit10 <mozahiv11@gmail.com>
2025-04-20 19:20:44 -0400

    Added the function for 50/30/20 rule and function to compare where to spend the budget

--- budget_analysis.py
+++ budget_analysis.py
@@ -7,3 +7,15 @@ class BudgetAnalysis:
     """
     def __init__(self, budget):
         self.budget = budget
+
+    def calculate_ideal(self):
+        """This will be used for the 50/30/20 rule."""
+        income = self.budget.monthly_income
+        return {
+            'need': income * 0.5,
+            'want': income * 0.3,
+            'saving': income * 0.2
+       }
+    
+    def compare_with_ideal(self):
+        """will compare user's expenses with ideal budget."""
