ab7da27d8f03d9bbafd12fdee03fe80bb5272879
GDW-cmd <GabrielDWinebrenner@gmail.com>
2025-04-20 19:17:18 -0400

    Init class

---
+++ budget_analysis.py
@@ -0,0 +1,9 @@
+class BudgetAnalysis:
+    """
+    Analyzes budget data by comparing it to the 50/30/20 budgeting rule.
+    
+    Attributes:
+        budget (Budget): Budget object 
+    """
+    def __init__(self, budget):
+        self.budget = budget
