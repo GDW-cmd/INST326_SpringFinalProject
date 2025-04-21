72009efb92bc949e8b85c955c8b2ee659c5a8f1e
Param2935 <pvprajapati1895@gmail.com>
2025-04-20 22:37:30 -0400

    strat with visualization and frontend class imports

---
+++ budget_frontend.py
@@ -0,0 +1,5 @@
+# This class creates a basic GUI for the program using streamlit.
+
+import streamlit as st
+from budget import Budget
+
---
+++ budget_visualization.py
@@ -0,0 +1,6 @@
+# This class generates a bar chart of the budget categories and their amounts using matplotlib.
+
+import matplotlib.pyplot as plt
+
+class BudgetVisualization:
+    pass
