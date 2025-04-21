1f931f04acc6b5074c6eda505ab1d5bedfe7ee84
Param2935 <pvprajapati1895@gmail.com>
2025-04-20 22:30:13 -0400

    set up a testcase

--- test_budget.py
+++ test_budget.py
@@ -3,3 +3,9 @@
 import unittest
 from budget import Budget 
 
+
+def setUp(self):
+    self.budget = Budget(5000) # initialize monthly income
+    # add expenses
+    self.budget.add_expense(100, 'need', 'Rent') 
+    self.budget.add_expense(200, 'want', 'Entertainment')
