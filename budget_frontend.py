2ae715a79be2fe3871f6b539ed774fb56e65781e
GDW-cmd <GabrielDWinebrenner@gmail.com>
2025-04-28 17:33:34 -0400

    Streamlit update, added basic title and monthly input then returns input to user

--- budget_frontend.py
+++ budget_frontend.py
@@ -3,3 +3,10 @@
 import streamlit as st
 from budget import Budget
 
+st.title("Basic Budget Tracker") #Title
+
+monthly_income = st.number_input("Monthly Income", min_value = 0.0, step = 0.01, format = "%0.2f") #Monthly income input
+st.write("The current number is ", monthly_income) #Shows monthly income input
+
+
+            
