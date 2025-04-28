b74962395c0952facd3fd7e6edbe843d4da7da25
GDW-cmd <GabrielDWinebrenner@gmail.com>
2025-04-28 17:53:15 -0400

    Streamlit update, Form that takes in expense name, then category and cost. Also button to submit form.

--- budget_frontend.py
+++ budget_frontend.py
@@ -8,5 +8,8 @@ st.title("Basic Budget Tracker") #Title
 monthly_income = st.number_input("Monthly Income", min_value = 0.0, step = 0.01, format = "%0.2f") #Monthly income input
 st.write("The current number is ", monthly_income) #Shows monthly income input
 
-
-            
+with st.form("expense_form"): #Expense form for all expense inputs, expense: cost, category[want, need, or saving] 
+    expense_name = st.text_input("Expense Name", placeholder="e.g., Rent, Groceries, Subscriptions") #Text input for the expense name/description
+    expense_category = st.selectbox("Category", options = ["Need", "Want", "Saving"], help = "Select whether this expense is a Need, Want, or Saving") #Select box for the expense category
+    expense_cost = st.number_input("Expense monthly cost", min_value = 0.0, step = 0.01,format = "%0.2f") #Number input for expense cost
+    submitted = st.form_submit_button("Add Expense") #Submit button
