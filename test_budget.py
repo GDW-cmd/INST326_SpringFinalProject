abbd536a2ee5eafc08576077be311b7ea6a1d4a3
GDW-cmd <GabrielDWinebrenner@gmail.com>
2025-05-08 19:09:34 -0400

    predefined dict to test methods

--- test_budget.py
+++ test_budget.py
@@ -5,4 +5,12 @@ from budget import Budget
 from budget_analysis import BudgetAnalysis
 
 class TestBudget(unittest.TestCase):
+    def setUp(self):
+        self.predefined_expense_dict = {
+            'rent': {'cost': 1500.00, 'category': 'Need'},
+            'groceries': {'cost': 320.00, 'category': 'Need'},
+            'subscriptions': {'want': 70.00, 'category': 'Want'},
+            'video games': {'want': 120.00, 'category': 'Want'},
+            'saving account': {'want': 300.00, 'category': 'Saving'}
+        }
     pass
