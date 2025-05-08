e154b954d36edf1c003f9279b2e3ddb2a0455516
GDW-cmd <GabrielDWinebrenner@gmail.com>
2025-05-08 19:02:24 -0400

    class TestBudget

--- test_budget.py
+++ test_budget.py
@@ -4,21 +4,5 @@ import unittest
 from budget import Budget 
 from budget_analysis import BudgetAnalysis
 
-
-def setUp(self):
-    self.budget = Budget(5000) # initialize monthly income
-    # add expenses
-    self.budget.add_expense(100, 'need', 'Rent') 
-    self.budget.add_expense(200, 'want', 'Entertainment')
-
-def test_get_total_savings(self):
-    '''  '''
-    pass 
-
-def test_get_total_needs(self):
-    '''    '''
-    pass
-
-def test_get_total_wants(self):
-    '''    '''
+class TestBudget(unittest.TestCase):
     pass
