9c5b77d30a5d37e4f051e69212165962d2e99d00
Param2935 <pvprajapati1895@gmail.com>
2025-05-10 12:11:53 -0400

    minor GUI changes

--- budget_frontend.py
+++ budget_frontend.py
@@ -3,38 +3,29 @@
 import streamlit as st
 from budget import Budget
 from budget_visualization import BudgetVisualization
+import pandas as pd
 
-
-
-# set a theme for the app
-st.markdown(
-"""
-<style>
-        body {
-            background-color: #FFF5F0;
-            color: #333333;
-<style> 
-""", unsafe_allow_html=True)
 
 
 #Initialize Empty Dictionary
 if 'expenses' not in st.session_state:
     st.session_state.expenses = {}
 
-st.title("Basic Budget Tracker") #Title 
+st.title("Personal Budget Tracker 💰") #Title 
 
 
 tab1, tab2, tab3 = st.tabs(["📝Input", "📊Spending Analysis", "Ideal Budget Comparition"]) #Creates tab for input and output(charts for now)
 
 with tab1:
     monthly_income = st.number_input("Monthly Income", min_value = 0.0, step = 0.01, format = "%0.2f") #Monthly income input
-    st.write("The current number is ", monthly_income) #Shows monthly income input
+    
+
 
     with st.form("expense_form"): #Expense form for all expense inputs, expense: cost, category[want, need, or saving] 
         expense_name = st.text_input("Expense Name", placeholder="e.g., Rent, Groceries, Subscriptions") #Text input for the expense name/description
-        expense_category = st.selectbox("Category", options = ["Need", "Want", "Saving"], help = "Select whether this expense is a Need, Want, or Saving") #Select box for the expense category
-        expense_cost = st.number_input("Expense monthly cost", min_value = 0.0, step = 0.01,format = "%0.2f") #Number input for expense cost
-        submitted = st.form_submit_button("Add Expense") #Submit button 
+        expense_category = st.selectbox("Categorize your expense", options = ["Need", "Want", "Saving"], help = "Select whether this expense is a Need, Want, or Saving") #Select box for the expense category
+        expense_cost = st.number_input("Enter monthly cost", min_value = 0.0, step = 0.01,format = "%0.2f") #Number input for expense cost
+        submitted = st.form_submit_button("Add") #Submit button 
 
         if submitted:
             if not expense_name:
