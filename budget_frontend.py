16fd73791c357fb9d536b0982fd66234e11c98b9
Param2935 <pvprajapati1895@gmail.com>
2025-05-10 12:57:11 -0400

    resolving more git merge errors

--- budget_frontend.py
+++ budget_frontend.py
@@ -16,7 +16,7 @@ if 'expenses_df' not in st.session_state:
 st.title("Personal Budget Tracker 💰") #Title 
 
 
-tab1, tab2, tab3 = st.tabs(["Input 📝", "Spending Analysis ", "Comparitive Analysis "]) #Creates tab for input and output(charts for now)
+tab1, tab2, tab3 = st.tabs(["Input 📝", "Spending Analysis 📊", "Comparitive Analysis "]) #Creates tab for input and output(charts for now)
 
 with tab1:
     monthly_income = st.number_input("Monthly Income", min_value = 0.0, step = 0.01, format = "%0.2f") #Monthly income input
@@ -41,8 +41,8 @@ with tab1:
             )
 
     st.subheader("Your Expenses")
-    if st.session_state.expenses:
-        st.write(st.session_state.expenses)
+    if not st.session_state.expenses_df.empty:
+        st.dataframe(st.session_state.expenses_df)
     else:
         st.write("None")
 
