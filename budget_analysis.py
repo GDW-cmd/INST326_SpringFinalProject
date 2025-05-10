71120faf464c615e7dae1470a4de3de64f359c77
GDW-cmd <GabrielDWinebrenner@gmail.com>
2025-05-09 23:36:57 -0400

    Fixed budget_analysis method

--- budget_analysis.py
+++ budget_analysis.py
@@ -1,7 +1,10 @@
 class BudgetAnalysis:
     """
     """
-    def __init__(self):
+    def __init__(self, expenses_dict, monthly_income):
+        self.expenses_dict = expenses_dict #User inputs of all expenses, name : category, cost
+        self.monthly_income = monthly_income #User input of income
+        self.ideal_budget = {'Needs': 0.5, 'Wants': 0.3, 'Savings': 0.2} #Dictionary of ideal budget
         pass
      
     def compare_with_ideal(self):
