3244285b3b318f8240a94892d2341cac65491049
GDW-cmd <GabrielDWinebrenner@gmail.com>
2025-05-10 00:01:31 -0400

    Changed function to get ideal spending

--- budget_analysis.py
+++ budget_analysis.py
@@ -7,10 +7,10 @@ class BudgetAnalysis:
     def __init__(self, expense_dict, monthly_income):
         self.budget = Budget(expense_dict) #User inputs of all expenses, name : category, cost
         self.monthly_income = monthly_income #User input of income
-        self.ideal_budget = {'Needs': 0.5, 'Wants': 0.3, 'Savings': 0.2} #Dictionary of ideal budget
+        self.ideal_percents = {'Needs': 0.5, 'Wants': 0.3, 'Savings': 0.2} #Dictionary of ideal budget
         pass
 
-    def user_category_percent_budget(self):
+    def get_ideal_spending(self):
         """
         Returns users budget percentages with dictionary, key (Category), value (Percentage)
         """
@@ -18,14 +18,18 @@ class BudgetAnalysis:
             return {'Needs': 0, 'Wants': 0, 'Savings': 0}
         totals = self.budget.get_all_totals()
         return {
-            'Needs': totals['Needs'] / self.monthly_income,
-            'Wants': totals['Wants'] / self.monthly_income,
-            'Savings': totals['Savings'] / self.monthly_income
-            }
+            'Needs': self.monthly_income * self.ideal_percents['Needs'],
+            'Wants': self.monthly_income * self.ideal_percents['Wants'],
+            'Savings': self.monthly_income * self.ideal_percents['Savings']
+        }
 
 
-     
     def compare_with_ideal(self):
         """will compare user's expenses with ideal budget and return values that will be used in chart."""
+
+        user_budget = self.user_category_percent_budget()
+        
+
+
         pass
         
