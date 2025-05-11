8936a5e489f874b16e0cecb0ad61447b67d54edd
MoGit10 <mozahiv11@gmail.com>
2025-05-11 18:56:39 -0400

    added test to check totals for each category and leftover money

--- test_budget.py
+++ test_budget.py
@@ -58,7 +58,32 @@ class TestBudget_analysis(unittest.TestCase):
 
         self.assertEqual(percent_diffs, expected_percent_diffs)
         self.assertEqual(dollar_diffs, expected_dollar_diffs) 
+
+
+class TestBudgetVisualization(unittest.TestCase): 
+    def setUp(self):
+        """Initialize test data before each test"""
+        self.expense_dict = {
+            'rent': {'cost': 1500.00, 'category': 'Need'}, #our need 
+            'groceries': {'cost': 320.00, 'category': 'Need'}, #our need 
+            'subscriptions': {'cost': 70.00, 'category': 'Want'}, #our want 
+            'video games': {'cost': 120.00, 'category': 'Want'}, #our want  
+            'saving account': {'cost': 300.00, 'category': 'Saving'} #our saving  
+        } 
+        # example income 
+        self.monthly_income = 4000
+
+        # makeing the budget analysis so we can test it
+        self.analysis = BudgetVisualization(self.expense_dict, self.monthly_income)
+
+    #to check if totals by category and leftover money are correct
+    def test_calculate_totals_cat(self):
+        result = self.visual.calculate_totals_cat()
+        expected_total = {'Needs': 1820.0, 'Wants': 190.0, 'Saving': 300.0, 'left': 1690.0
+        }
+        self.assertEqual(result, expected_total)
     
+
 
 if __name__ == '__main__':
     unittest.main() 
